f031e0272cac4f49cb5c267ae44182c7bfabaf71
Minh_Vnit <hungquocminh1989@gmail.com>
2017-07-28 16:11:05 +0700

    Init source

---
+++ user_config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('ACW_PROJECT', 'simple_framework');
+define('ACW_BASE_URL', 'http://192.168.1.15/simple_framework/');	
+define('AKAGANE_SALT', 'akg1$%');
+define('ACW_SESSION_PARENT_DIR', 'C://acwork_session/');
+define('ACW_SESSION_DIR', ACW_SESSION_PARENT_DIR . ACW_PROJECT . '/');
